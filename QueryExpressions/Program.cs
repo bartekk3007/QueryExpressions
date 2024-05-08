8ea1e95d33de09d145447e83de7547d83234613a
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-05-08 23:42:47 +0200

    Add files via upload

---
+++ QueryExpressions/Program.cs
@@ -0,0 +1,38 @@
+﻿using ListMethods;
+
+namespace QueryExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Winner> winnerList = new List<Winner>
+            {
+                new Winner("Argentina", new[] {1978, 1986, 2022}),
+                new Winner("Brazil", new[] {1958, 1962, 1970, 1994, 2002}),
+                new Winner("England", new[]{1970}),
+                new Winner("France", new[]{1998, 2018}),
+                new Winner("Germany", new[] {1954, 1974, 1990, 2014}),
+                new Winner("Italy", new[]{1934, 1938, 1982, 2006}),
+                new Winner("Spain", new[]{2010}),
+                new Winner("Uruguay", new[]{1930, 1950}),
+            };
+            winnerList.Sort();
+            Print(winnerList);
+
+            IEnumerable<Winner> bestWinners =
+                from winner in winnerList
+                where winner.Years.Length >= 3
+                orderby winner.Country ascending
+                select winner;
+            Print(bestWinners);
+        }
+        private static void Print<T>(IEnumerable<T> elems)
+        {
+            foreach (T elem in elems)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+}
---
+++ QueryExpressions/Winner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListMethods
+{
+    internal class Winner : IComparable<Winner>
+    {
+        public string Country { get; set; }
+        public int[] Years { get; set; }
+        public Winner(string country, int[] years)
+        {
+            Country = country;
+            Years = years;
+        }
+        public override string ToString()
+        {
+            string result = Country;
+            foreach (int year in Years)
+            {
+                result += " " + year;
+            }
+            return result;
+        }
+        public int CompareTo(Winner? y)
+        {
+            if (y == null)
+            {
+                return 1;
+            }
+            else if (Years.Length != y.Years.Length)
+            {
+                return y.Years.Length - Years.Length;
+            }
+            else
+            {
+                return string.Compare(Country, y.Country);
+            }
+        }
+    }
+}
